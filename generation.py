9d7acdafe88efacd67433495061165a4a5887650
radiating-pumpkin0Zed <a2zthisisforearningmoney@gmail.com>
2026-03-23 02:47:48 +0000

    predator balance

--- generation.py
+++ generation.py
@@ -90,7 +90,7 @@ def generate_creature(word, grid, creatures, GRID_SIZE, creature_area_is_free, d
         "cooldown": 0,
         "age": 0,
         "max_age": random.randint(1000, 2000),
-        "is_predator": random.random() < 0.15
+        "is_predator": random.random() < 0.08
     }
 
     draw_creature(creature, grid, GRID_SIZE)
